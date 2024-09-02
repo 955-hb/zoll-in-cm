23c169d4e85439afb06ca95effb7456b60921021
Hannes Bröhl <hannes.broehl@icloud.com>
2024-09-02 15:14:22 +0200

    umrechnungs-Funktion in einfach

---
+++ main.py
@@ -0,0 +1,13 @@
+laenge_zoll = float(input("Bitte Länge in Zoll eingeben:"))
+laenge_cm = laenge_zoll * 2.54
+print("Ergebnis:", laenge_cm , "Zentimeter")
+
+#value = float(input("How much has the customer spent? "))
+#discount = 0.05
+
+#final_cost = value - (value * discount)
+#print(round(final_cost, 2))
+
+#The float() method is surrounded by the input() method.
+#The float() method converts the string value returned by input() into a floating-point number.
+#This allows us to multiply “value” and “discount” because they are both numbers.
